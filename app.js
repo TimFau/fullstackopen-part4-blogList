b30a685a38baac98e1e18f2c047a878981dc46c3
Tim Fau <dev@timfau.com>
2023-06-14 22:38:56 -0400

    Exercise 4.18
    # Implement token-based authentication

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blogs')
 const User = require('./models/user');
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
